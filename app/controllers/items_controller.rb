c0bcdf4d6ae9a1479f85a6dbbc9f75078fbaa25b
Alexandra Borge <alexandraroseborge@gmail.com>
2019-07-21 15:43:27 -0400

    Ad updating functionality - if no values are changed, then keep the old value

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
 
   def new
     @day = params[:day]
-    @day_id = Day.all.find { |day| day[:description] == @day }[:id]
+    @day_id = Day.find_by(description: @day)[:id]
   end
 
   def create
@@ -21,6 +21,25 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item_title = Item.all.find { |item| item[:id] == params[:id].to_i }[:title]
+    @item_id = params[:id]
+    @item = Item.find_by(id: @item_id)
+    @item_title = @item[:title]
+    @item_description = @item[:description]
+  end
+
+  def update
+    title = params[:title]
+    description = params[:description]
+    previous_title = params[:previous_title]
+    previous_description = params[:previous_description]
+
+    new_title = title.blank? ? previous_title : title
+    new_description = description.blank? ? previous_description : description
+
+    item = Item.find_by(id: params[:id])
+    item.update(title: new_title)
+    item.update(description: new_description)
+
+    redirect_to('/')
   end
 end
